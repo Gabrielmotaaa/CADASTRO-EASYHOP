7b43217d158f253035ab4f8947d7cf27a89d5e81
Gabrielmotaaa <luizgabrielmotasil@gmail.com>
2024-07-30 20:58:42 -0300

    Adicionar arquivos de projeto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CADASTRO_EASYHOP.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
